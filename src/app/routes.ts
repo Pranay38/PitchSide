55e33ec2452715ea8f376c218935b9e1bbe718b4
Pranay Agrawal <100509183+Pranay38@users.noreply.github.com>
2026-03-01 04:10:00 +0530

    Fix: re-add /compare route to routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { BlogPostPage } from "./pages/BlogPostPage";
 import { AdminPage } from "./pages/AdminPage";
+import { ComparePage } from "./pages/ComparePage";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
   {
     path: "/post/:id",
     Component: BlogPostPage,
+  },
+  {
+    path: "/compare",
+    Component: ComparePage,
   },
   {
     path: "/pitchside-manage-x7k9",
